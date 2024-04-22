e3df6b814a1fdbf97d1c84df8067398811e35385
Thin-Thin-Moe <thinnthinnmoe1996@gmail.com>
2024-04-22 23:07:39 +0630

    db connection

--- TTMDotNetCore.ConsoleApp/Program.cs
+++ TTMDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+// nuget
+// SqlConnection
+
+// Ctrl + .
+// F10
+// F11
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = ".";
+stringBuilder.InitialCatalog = "TTMDotNetCore";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+connection.Open();
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+    Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+}
+
+connection.Close();
+
+Console.ReadLine();
