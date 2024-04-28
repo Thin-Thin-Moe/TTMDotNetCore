1ba69e5b5a0055167c597ba37138c2ee40a456b1
Thin-Thin-Moe <thinnthinnmoe1996@gmail.com>
2024-04-28 20:40:22 +0630

    EFCore crud

--- TTMDotNetCore.ConsoleApp/ConnectionStrings.cs
+++ TTMDotNetCore.ConsoleApp/ConnectionStrings.cs
@@ -14,7 +14,8 @@ namespace TTMDotNetCore.ConsoleApp
             DataSource = ".",
             InitialCatalog = "TTMDotNetCore",
             UserID = "sa",
-            Password = "sa@123"
+            Password = "sa@123",
+            TrustServerCertificate = true
         };
     }
 }
