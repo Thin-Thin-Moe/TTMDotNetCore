72c88bd3cbddc5e62c7c33db22d50d1cc7d7e2f6
Thin-Thin-Moe <thinnthinnmoe1996@gmail.com>
2024-05-22 23:23:26 +0630

    Get Incompatible Foods

---
+++ TTMDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+string jsonStr = await File.ReadAllTextAsync("IncompatibleFood.json");
+var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+Console.WriteLine(model.Tbl_IncompatibleFood);
+
+foreach (var food in model.Tbl_IncompatibleFood)
+{
+    Console.WriteLine(food.FoodA);
+}
+Console.ReadLine();
+
+
+public class MainDto
+{
+    public Tbl_Incompatiblefood[] Tbl_IncompatibleFood { get; set; }
+}
+
+public class Tbl_Incompatiblefood
+{
+    public int Id { get; set; }
+    public string FoodA { get; set; }
+    public string FoodB { get; set; }
+    public string Description { get; set; }
+}
---
+++ TTMDotNetCore.RestApiWithNLayer/Features/IncompatibleFood/IncompatibleFoodController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace TTMDotNetCore.RestApiWithNLayer.Features.IncompatibleFood;
+
+[Route("api/[controller]")]
+[ApiController]
+public class IncompatibleFoodController : ControllerBase
+{
+    private async Task<IncompatibleFood> GetDataAsync()
+    {
+        string jsonStr = await System.IO.File.ReadAllTextAsync("IncompatibleFood.json");
+        var model = JsonConvert.DeserializeObject<IncompatibleFood>(jsonStr);
+        return model;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetIncompatibleFood(string? description)
+    {
+        var model = await GetDataAsync();
+        if (description is null)
+        {
+            return Ok(model);
+        }
+
+        var filteredFoods = model.Tbl_IncompatibleFood.Where(food => food.Description.Trim().Contains(description.Trim()));
+
+        return Ok(filteredFoods);
+    }
+
+    public class IncompatibleFood
+    {
+        public Tbl_Incompatiblefood[] Tbl_IncompatibleFood { get; set; }
+    }
+
+    public class Tbl_Incompatiblefood
+    {
+        public int Id { get; set; }
+        public string FoodA { get; set; }
+        public string FoodB { get; set; }
+        public string Description { get; set; }
+    }
+}
